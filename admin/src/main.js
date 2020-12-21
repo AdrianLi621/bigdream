5498882e6e9483ff976b5d00330236e566c92fb1
e <1772702063@qq.com>
2020-12-21 12:00:46 +0800

    'ant-design'

--- admin/src/main.js
+++ admin/src/main.js
@@ -4,7 +4,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/element.js'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
+Vue.use(Antd)
 Vue.config.productionTip = false
 
 new Vue({
