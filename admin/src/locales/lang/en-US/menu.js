8142b6f8ed639a9a95dcf03591d3aca699705a88
e <1772702063@qq.com>
2021-02-24 13:56:16 +0800

    admin

---
+++ admin/src/locales/lang/en-US/menu.js
@@ -0,0 +1,13 @@
+export default {
+    'menu.welcome': 'Welcome',
+    'menu.home': 'Home',
+    'menu.dashboard': 'Dashboard',
+    'menu.dashboard.analysis': 'Analysis',
+    'menu.dashboard.monitor': 'Monitor',
+    'menu.dashboard.workplace': 'Workplace',
+    'menu.account': 'Account',
+    'menu.account.center': 'Account Center',
+    'menu.account.settings': 'Account Settings',
+    'menu.account.trigger': 'Trigger Error',
+    'menu.account.logout': 'Logout'
+}
